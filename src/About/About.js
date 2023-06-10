bca3c49293cfb4e852092b570873cc3b9e25b273
Amr <amruser97@gmail.com>
2023-06-10 18:15:48 +0300

    added about section

--- src/About/About.js
+++ src/About/About.js
@@ -37,7 +37,25 @@ const About = () => {
               Learn more &rarr;
             </a>
           </div>
-          <div className="col-1-of-2">col-1-of-2</div>
+          <div className="col-1-of-2">
+            <div className="composition">
+              <img
+                src="./img/nat-1-large.jpg"
+                alt="nature"
+                className="composition__photo composition__photo--p1"
+              />
+              <img
+                src="./img/nat-2-large.jpg"
+                alt="nature"
+                className="composition__photo composition__photo--p2"
+              />
+              <img
+                src="./img/nat-3-large.jpg"
+                alt="nature"
+                className="composition__photo composition__photo--p3"
+              />
+            </div>
+          </div>
         </div>
       </section>
       {/* <div className="float-rows">
